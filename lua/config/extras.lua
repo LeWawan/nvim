bad8f6e09acb3e1f4f8195e2ddd3ebee39861e94
erwan.kreutz <erwan.kreutz@delupay.com>
2025-04-22 00:00:06 +0200

    refactor(wip): update configs

--- lua/config/extras.lua
+++ lua/config/extras.lua
@@ -1,4 +1,4 @@
-vim.api.nvim_create_user_command("MemoryUsage", function()
-	local mem = collectgarbage("count")
-	print(string.format("Memory usage: %.2f MB", mem / 1024))
+vim.api.nvim_create_user_command('MemoryUsage', function()
+  local mem = collectgarbage 'count'
+  print(string.format('Memory usage: %.2f MB', mem / 1024))
 end, {})
