73cc3b0bc3768c05c142c5800b07a2cd5ae8605f
erwan.kreutz <erwan.kreutz@delupay.com>
2025-04-21 19:19:14 +0200

    feat(wip): rework all the conf with kickstart.nvim model

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -1,22 +1,30 @@
 return {
   {
-    "stevearc/conform.nvim",
+    'stevearc/conform.nvim',
     config = function()
-      require("conform").setup({
+      require('conform').setup {
         formatters_by_ft = {
-          javascript = { "prettier" },
-          typescript = { "prettier" },
-          vue = { "prettier" },
-          lua = { "stylua" },
+          javascript = { 'prettier' },
+          typescript = { 'prettier' },
+          vue = { 'prettier' },
+          lua = { 'stylua' },
         },
         format_on_save = {
           timeout_ms = 1000,
-          lsp_fallback = "fallback",
+          lsp_fallback = 'fallback',
         },
-      })
+      }
+
+      require('ts-error-translator').setup {}
     end,
   },
   {
-    "dmmulroy/ts-error-translator.nvim"
-  }
+    'dmmulroy/ts-error-translator.nvim',
+    config = function()
+      vim.lsp.handlers['textDocument/publishDiagnostics'] = function(err, result, ctx)
+        require('ts-error-translator').translate_diagnostics(err, result, ctx)
+        vim.lsp.diagnostic.on_publish_diagnostics(err, result, ctx)
+      end
+    end,
+  },
 }
