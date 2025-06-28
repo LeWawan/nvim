4913658f3bc2299f5aeaf4953128547a18d7df70
LeWawan <erwankreutz@gmail.com>
2025-06-28 16:36:47 +0200

    feat: migrate from copilot.vim to copilot.lua

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,5 +1,25 @@
 return {
   {
-    'github/copilot.vim',
+    'zbirenbaum/copilot.lua',
+    cmd = 'Copilot',
+    event = 'InsertEnter',
+    config = function()
+      require('copilot').setup {
+        suggestion = {
+          enabled = true,
+          auto_trigger = true,
+          hide_during_completion = true,
+          debounce = 75,
+          trigger_on_accept = true,
+          keymap = {
+            accept = '<Tab>',
+            accept_word = false,
+            accept_line = false,
+            next = '<C-]>',
+            prev = '<C-[>',
+          },
+        },
+      }
+    end,
   },
 }
