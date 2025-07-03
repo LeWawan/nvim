edcef0595adc5c6bbb726af25ae5d3bc5c66dc44
LeWawan <erwankreutz@gmail.com>
2025-07-03 12:26:06 +0200

    refactor: update Lazy initialisation config

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -18,12 +18,13 @@ vim.opt.rtp:prepend(lazypath)
 -- Setup lazy.nvim
 require('lazy').setup {
   spec = {
-    -- { 'LazyVim/LazyVim' },
+    { 'LazyVim/LazyVim' },
+    -- { 'LazyVim/LazyVim', import = 'lazyvim.plugins' },
     -- { import = "lazyvim.plugins.extras.linting.eslint" },
     -- { import = "lazyvim.plugins.extras.formatting.prettier" },
     -- { import = "lazyvim.plugins.extras.lang.typescript" },
     -- { import = "lazyvim.plugins.extras.lang.json" },
-    -- { import = "lazyvim.plugins.extras.lang.tailwind" },
+    -- { import = 'lazyvim.plugins.extras.lang.tailwind' },
     --
     { import = 'plugins' },
   },
