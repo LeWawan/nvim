010667b26e644375c01cc031dbf6801f5784e6ff
LeWawan <erwankreutz@gmail.com>
2025-07-03 12:25:44 +0200

    chore: add some test files

---
+++ tests/file.ts
@@ -0,0 +1,6 @@
+console.log('This is a test file.');
+
+
+const func = (a: number, b: string): number => {
+  return a + b;
+}
---
+++ tests/file.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+console.log('file.vue');
+
+</script>
+
+<template>
+  <div>
+    <h1>File Component</h1>
+    <p>This is a simple Vue component.</p>
+  </div>
+</template>
+
