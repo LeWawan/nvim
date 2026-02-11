d4f37b59e6d76ec71b286d72017775a48421eec6
Erwan KREUTZ <erwankreutz@gmail.com>
2026-02-11 23:53:41 +0100

    chore: update dependencies origin after repo update
    mason repos have moved from williamboman group to mason-org

--- lua/plugins/lsp.vanilla.lua
+++ lua/plugins/lsp.vanilla.lua
@@ -12,8 +12,11 @@ return {
   {
     'neovim/nvim-lspconfig',
     dependencies = {
-      { 'williamboman/mason.nvim', opts = {} },
-      'williamboman/mason-lspconfig.nvim',
+      -- Automatically install LSPs and related tools to stdpath for Neovim
+      -- Mason must be loaded before its dependents so we need to set it up here.
+      -- NOTE: `opts = {}` is the same as calling `require('mason').setup({})`
+      { 'mason-org/mason.nvim', opts = {} },
+      'mason-org/mason-lspconfig.nvim',
       'WhoIsSethDaniel/mason-tool-installer.nvim',
 
       {
