473697cb8afc41fe7e4ac30c1365056e9eb8f6f6
LeWawan <erwankreutz@gmail.com>
2026-03-03 00:27:43 +0100

    fix: undefined global "vim"

--- lua/plugins/lsp.vanilla.lua
+++ lua/plugins/lsp.vanilla.lua
@@ -25,7 +25,9 @@ return {
         opts = {},
       },
 
-      'saghen/blink.cmp',
+      -- {
+      --   'saghen/blink.cmp',
+      -- },
 
       'tpope/vim-rails', -- Essential pour Rails
     },
@@ -139,6 +141,38 @@ return {
       ---@type table<string, vim.lsp.Config>
       local servers = {
         lua_ls = {
+          on_init = function(client)
+            if client.workspace_folders then
+              local path = client.workspace_folders[1].name
+              if
+                path ~= vim.fn.stdpath('config')
+                and (vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc'))
+              then
+                return
+              end
+            end
+
+            client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+              runtime = {
+                version = 'LuaJIT',
+                path = {
+                  'lua/?.lua',
+                  'lua/?/init.lua',
+                },
+              },
+              workspace = {
+                checkThirdParty = false,
+                library = {
+                  vim.env.VIMRUNTIME,
+                  -- Depending on the usage, you might want to add additional paths
+                  -- here.
+                  '${3rd}/luv/library',
+                  -- '${3rd}/busted/library',
+                },
+              },
+            })
+          end,
+          cmd = { 'lua-language-server' },
           settings = {
             Lua = {
               completion = {
